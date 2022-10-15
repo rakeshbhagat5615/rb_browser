f116660e1db26585499e624edde701f18ed337c1
Rakesh Bhagat <rakeshbhagat.rb5615@gmail.com>
2022-10-15 14:25:24 +0530

    Base App

--- app/src/main/java/com/rb/rbbrowser/MainActivity.java
+++ app/src/main/java/com/rb/rbbrowser/MainActivity.java
@@ -3,6 +3,12 @@ package com.rb.rbbrowser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.activity_main_web);
+        EditText urlET = findViewById(R.id.activity_main_url_et);
+        Button goBT = findViewById(R.id.activity_main_go_bt);
+
+        webView.loadUrl("https://www.google.com/");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        goBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlContent = urlET.getText().toString();
+                if(!TextUtils.isEmpty(urlContent)){
+                    String googleSearchURL = "https://www.google.com/search?q=" + urlContent;
+                    webView.loadUrl(googleSearchURL);
+                }
+            }
+        });
     }
 }
